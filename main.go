741c2d036d0ea433b6f9e1d0e2ebe7c78f8a5f1d
Mohammad_Alhallaq <mohammad.alhallaq1@gmail.com>
2023-10-22 11:01:32 +0300

    add test cases

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 )
 
 func main() {
+	router := setupRouter()
+	router.Run(":8080")
+}
+
+func setupRouter() *gin.Engine {
 	router := gin.Default()
 
 	router.GET("/ping", health)         // /ping endpint provide the info about the health of server
 	router.POST("/xml/yaml", xmlToYaml) // /xml/yaml endpoint convert the provided xml into yaml
 	router.GET("/xml/isValid", isXml)   //  /xml/isValid used to check if a given xml data is valid or not
 
-	router.Run(":8080")
+	return router
 }
